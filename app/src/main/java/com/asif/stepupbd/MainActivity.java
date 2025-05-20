0bcc4222a0b722dd79c869bce66689c6e14d426f
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-05-20 18:05:13 +0600

    Commit 3

--- app/src/main/java/com/asif/stepupbd/MainActivity.java
+++ app/src/main/java/com/asif/stepupbd/MainActivity.java
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +19,7 @@ import java.security.AccessController;
 public class MainActivity extends AppCompatActivity {
 
     TextView map;
+    Button buttonMap;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -26,20 +29,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         map = findViewById(R.id.map);
+        buttonMap = findViewById(R.id.buttonMap);
 
         // google map code --> jei button a dorkar oi button er set on click er moddhe diye dibo
         // map er dependency holo : implementation("com.google.maps.android:android-maps-utils:3.10.0")
 
-
+/*
 
         Uri gmmIntentUri = Uri.parse("geo:0,0?q="+"Pingna High School ");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         map.getContext().startActivity(mapIntent);
 
-
+ */
 
         //=================================================================
+
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q="+"Pingna High School ");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                buttonMap.getContext().startActivity(mapIntent);
+
+            }
+        });
+
 
     }
 }
