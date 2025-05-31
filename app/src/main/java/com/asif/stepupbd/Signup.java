c552a803871d54cdca3ff7cb9207fcd35d22c691
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-05-31 21:30:38 +0600

    Commit 8.3

--- app/src/main/java/com/asif/stepupbd/Signup.java
+++ app/src/main/java/com/asif/stepupbd/Signup.java
@@ -1,6 +1,15 @@
 package com.asif.stepupbd;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +17,59 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.io.ByteArrayOutputStream;
+
 public class Signup extends AppCompatActivity {
+
+    ImageView imageProfile;
+    TextView tvChangePhoto;
+    TextInputEditText inputEmail, inputPassword, inputName;
+    Button buttonSignup, buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        imageProfile = findViewById(R.id.imageProfile);
+        tvChangePhoto = findViewById(R.id.tvChangePhoto);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputName = findViewById(R.id.inputName);
+        buttonSignup = findViewById(R.id.buttonSignup);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, Login.class));
+                finish();
+            }
+        });
+
+        buttonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+                String name = inputName.getText().toString();
+
+                BitmapDrawable bitmapDrawable = (BitmapDrawable) imageProfile.getDrawable();
+                Bitmap bitmap = bitmapDrawable.getBitmap();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+
+                byte[] imageBytes = outputStream.toByteArray();
+                String image = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+
+            }
+        });
+
 
     }
 }
