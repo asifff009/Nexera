8e10e7c98764f8c58a0b6f5322fcd2f03a87b655
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-06-12 21:01:17 +0600

    Logout setup 2.0 - EmployerrPage

--- app/src/main/java/com/asif/stepupbd/EmployerrPage.java
+++ app/src/main/java/com/asif/stepupbd/EmployerrPage.java
@@ -2,23 +2,33 @@ package com.asif.stepupbd;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.asif.stepupbd.R;
 
 public class EmployerrPage extends AppCompatActivity {
+
+    Button buttonNxt2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employerr_page);
 
-        Button logoutBtn = findViewById(R.id.logoutBtn);
-        logoutBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(EmployerrPage.this, com.asif.stepupbd.LoginActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
+        buttonNxt2 = findViewById(R.id.buttonNxt2);
+
+        buttonNxt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intenttt = new Intent(EmployerrPage.this, Logout.class);
+                startActivity(intenttt);
+
+            }
         });
+
+
+
+
     }
 }
