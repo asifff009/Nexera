457210476cc22c74fbfa015b94bd21468eb88ae7
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-07-20 23:09:55 +0600

    Update and Delete Button add

---
+++ app/src/main/java/com/asif/stepupbd/JobDetailsActivity2.java
@@ -0,0 +1,27 @@
+package com.asif.stepupbd;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JobDetailsActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_details2);
+
+        TextView title = findViewById(R.id.textTitle);
+        TextView desc = findViewById(R.id.textDescription);
+        TextView exp = findViewById(R.id.textExperience);
+        TextView dur = findViewById(R.id.textDuration);
+        TextView loc = findViewById(R.id.textLocation);
+        TextView contact = findViewById(R.id.textContact);
+
+        title.setText("Title: " + getIntent().getStringExtra("title"));
+        desc.setText("Description: " + getIntent().getStringExtra("description"));
+        exp.setText("Experience: " + getIntent().getStringExtra("experience"));
+        dur.setText("Duration: " + getIntent().getStringExtra("duration"));
+        loc.setText("Location: " + getIntent().getStringExtra("location"));
+        contact.setText("Contact: " + getIntent().getStringExtra("contact"));
+    }
+}
